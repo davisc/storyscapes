30bae217864414a5a2e276896aa4348a5a7e5a74
Jorge <jorgemg0327@gmail.com>
2016-04-06 15:53:16 -0500

    Organizing files in main folder

--- init_db.sh
+++ init_db.sh
@@ -3,7 +3,7 @@ echo "------ Create database tables ------"
 python manage.py syncdb --noinput
 
 echo "------ create default admin user ------"
-echo "from geonode.people.models import Profile; Profile.objects.create_superuser('admin', 'admin@boundlessgeo.com', 'admin')" | python manage.py shell
+echo "from geonode.people.models import Profile; Profile.objects.create_superuser('admin', 'admin@mapstory.com', 'admin')" | python manage.py shell
 
 echo "------ starting gunicorn  ------"
 gunicorn mapstory.wsgi --workers 2
